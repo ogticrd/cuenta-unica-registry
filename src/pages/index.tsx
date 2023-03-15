fb0087383ee5abf5defa63acfb07db7e92d12c8a
je1999 <je99alvarez@gmail.com>
2023-03-15 16:40:49 -0400

    [ADD] pages auth

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,7 @@
-import { ThemeProvider } from '@mui/material/styles';
-import CssBaseline from '@mui/material/CssBaseline';
-
-import { theme } from '../themes';
-
-import Layout from '../components/layout'
-
 import AuthHome from './auth'
 
 export default function Home() {
   return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <Layout>
-        <AuthHome />
-      </Layout>
-    </ThemeProvider>
+    <AuthHome />
   )
 }
