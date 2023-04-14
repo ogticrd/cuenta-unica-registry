9070f791f82034b515363cb94fc22c4b0122eb0a
je1999 <je99alvarez@gmail.com>
2023-04-14 19:58:08 -0400

    [ADD] favicon and head title

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,9 +7,13 @@ import { theme } from '../themes';
 import Layout from '../components/layout'
 
 import '@/styles/globals.css'
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }: AppProps) {
   return <>
+    <Head>
+      <title>Plataforma Única de Autenticación</title>
+    </Head>
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <Layout>
