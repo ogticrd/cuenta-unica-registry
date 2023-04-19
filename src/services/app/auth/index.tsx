82b53fcde01148797f4d41e7b854ebb8f6c80ca3
je1999 <je99alvarez@gmail.com>
2023-04-18 20:49:32 -0400

    [ADD] validation cedula

--- src/services/app/auth/index.tsx
+++ src/services/app/auth/index.tsx
@@ -2,4 +2,5 @@ import { authAxios } from "../../../configs/axios";
 
 export namespace authApi {
     export const post = async (data: any) => await authAxios.post(`/auth/signup`, data)
+    export const getVerifyUser = async (cedula: string) => await authAxios.get(`/auth/validations/users/existence?username=${cedula}`)
 }
