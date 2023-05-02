7f7c17774180f1f7faabe93d68d1d62fbc92e12e
Marluan Espiritusanto <marluanespiritusantoguerrero@gmail.com>
2023-05-02 22:04:03 +0200

    ref: public runtine env implemented

--- src/pages/api/citizens/[cedula].ts
+++ src/pages/api/citizens/[cedula].ts
@@ -1,20 +1,23 @@
 import { NextApiRequest, NextApiResponse } from "next/types";
+import getConfig from "next/config";
 import axios from "axios";
 
 import { CitizensBasicInformationResponse } from "../types";
+
+const { publicRuntimeConfig } = getConfig();
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<CitizensBasicInformationResponse>
 ): Promise<void> {
   const http = axios.create({
-    baseURL: process.env["NEXT_PUBLIC_CEDULA_API"],
+    baseURL: publicRuntimeConfig.NEXT_PUBLIC_CEDULA_API,
   });
 
   const { cedula } = req.query;
 
   const { data: citizen } = await http.get<CitizensBasicInformationResponse>(
-    `/${cedula}/info/basic?api-key=${process.env["NEXT_PUBLIC_CEDULA_API_KEY"]}`
+    `/${cedula}/info/basic?api-key=${publicRuntimeConfig.NEXT_PUBLIC_CEDULA_API_KEY}`
   );
 
   res.status(200).json(citizen);
