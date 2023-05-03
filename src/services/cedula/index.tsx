a559409ee92da326f5fb9611ca775b6c34f7753e
Marluan Espiritusanto <marluanespiritusantoguerrero@gmail.com>
2023-05-03 18:37:03 +0200

    ref(code): standard applied; docker image refactored

--- src/services/cedula/index.tsx
+++
@@ -1,8 +0,0 @@
-import { cedulaAxios } from "../../configs/axios";
-
-export namespace cedulaApi {
-  export const get = async (cedula: string) =>
-    await cedulaAxios.get(
-      `${cedula}/info/basic?api-key=${process.env.NEXT_PUBLIC_CEDULA_API_KEY}`
-    );
-}
