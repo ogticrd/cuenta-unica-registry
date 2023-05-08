0209d2812f2588be63e0e09d5fe3b4f848ca518a
Marluan Espiritusanto <marluanespiritusantoguerrero@gmail.com>
2023-05-08 22:40:02 +0200

    fix: api auth header

--- next.config.js
+++ next.config.js
@@ -7,6 +7,7 @@ const nextConfig = {
     NEXT_PUBLIC_CEDULA_API: process.env.NEXT_PUBLIC_CEDULA_API,
     NEXT_PUBLIC_CEDULA_API_KEY: process.env.NEXT_PUBLIC_CEDULA_API_KEY,
     NEXT_PUBLIC_SITE_KEY: process.env.NEXT_PUBLIC_SITE_KEY,
+    NEXT_PUBLIC_COOKIE_KEY: process.env.NEXT_PUBLIC_COOKIE_KEY,
   },
   reactStrictMode: false,
 };
