3d21369985e2095812ed32a1fd25f761480a200c
Gustavo Valverde <g.valverde02@gmail.com>
2023-05-14 12:28:21 -0400

    refactor(biometry): reopen WebSocket  on each retry

--- src/components/biometric/face-liveness-detector.tsx
+++ src/components/biometric/face-liveness-detector.tsx
@@ -3,31 +3,27 @@ import { Loader, ThemeProvider } from '@aws-amplify/ui-react';
 import React from 'react';
 import { useState, useEffect } from 'react';
 
+import { AlertErrorMessage } from '../elements/alert';
 import { defaultLivenessDisplayText } from './displayText';
 
 export function LivenessQuickStartReact({ handleNextForm, cedula }: any) {
   const next = handleNextForm;
   const id = cedula;
   const [loading, setLoading] = useState<boolean>(true);
-  const [error, setError] = useState<string | null>(null);
-  const [sessionId, setSessionId] = useState<{
-    sessionId: string;
-  } | null>(null);
+  const [error, setError] = useState<Error | null>(null);
+  const [sessionId, setSessionId] = useState<string | null>(null);
 
-  useEffect(() => {
-    const fetchCreateLiveness = async () => {
-      const response = await fetch(`/api/biometric`, { method: 'POST' });
-      const { sessionId } = await response.json();
+  const fetchCreateLiveness = async () => {
+    const response = await fetch(`/api/biometric`, { method: 'POST' });
+    const { sessionId } = await response.json();
 
-      setSessionId(sessionId);
-      setLoading(false);
-    };
-
-    fetchCreateLiveness();
-  }, []);
+    setSessionId(sessionId);
+    setLoading(false);
+  };
 
   const onUserCancel = () => {
-    setError(null);
+    setSessionId(null);
+    fetchCreateLiveness();
   };
 
   const handleAnalysisComplete = async () => {
@@ -36,30 +32,48 @@ export function LivenessQuickStartReact({ handleNextForm, cedula }: any) {
     );
     const data = await response.json();
 
-    if (data.match === true) {
+    if (data.isMatch === true) {
       next();
     } else {
-      setError(error);
+      setError(data.error);
+      setSessionId(null);
+      fetchCreateLiveness();
     }
   };
 
+  useEffect(() => {
+    fetchCreateLiveness().catch((error) => {
+      console.error(error);
+      setError(error);
+    });
+  }, []);
+
   return (
-    <ThemeProvider>
-      {loading ? (
-        <Loader />
-      ) : (
-        sessionId && (
-          <FaceLivenessDetector
-            sessionId={sessionId?.sessionId}
-            region="us-east-1"
-            onUserCancel={onUserCancel}
-            onError={(error) => setError(error.message)}
-            onAnalysisComplete={handleAnalysisComplete}
-            disableInstructionScreen={true}
-            displayText={defaultLivenessDisplayText}
-          />
-        )
+    <>
+      {error && (
+        <AlertErrorMessage
+          type="info"
+          message="No se ha podido validar correctamente la identidad."
+        />
       )}
-    </ThemeProvider>
+      <br />
+      <ThemeProvider>
+        {loading ? (
+          <Loader />
+        ) : (
+          sessionId && (
+            <FaceLivenessDetector
+              sessionId={sessionId}
+              region="us-east-1"
+              onUserCancel={onUserCancel}
+              onError={(error) => setError(error)}
+              onAnalysisComplete={handleAnalysisComplete}
+              disableInstructionScreen={true}
+              displayText={defaultLivenessDisplayText}
+            />
+          )
+        )}
+      </ThemeProvider>
+    </>
   );
 }
