d4b65cb6bba00c2fd80519d0172a0238bd2829f6
Gustavo Valverde <gustavo@iterativo.do>
2023-05-15 20:29:50 -0400

    feat(registry): show password strenght to user (#27)
    This change was made by @JE1999, I just updated it with changes from `master`

--- src/components/form/input/index.tsx
+++ src/components/form/input/index.tsx
@@ -1,8 +1,8 @@
-import InfoOutlinedIcon from "@mui/icons-material/InfoOutlined";
-import Tooltip, { TooltipProps, tooltipClasses } from "@mui/material/Tooltip";
-import { FormControl, InputLabel, Typography } from "@mui/material";
-import { styled } from "@mui/material/styles";
-import { IconButton } from "@mui/material";
+import InfoOutlinedIcon from '@mui/icons-material/InfoOutlined';
+import Tooltip, { TooltipProps, tooltipClasses } from '@mui/material/Tooltip';
+import { FormControl, InputLabel, Typography } from '@mui/material';
+import { styled } from '@mui/material/styles';
+import { IconButton } from '@mui/material';
 
 interface IProps {
   label?: string;
@@ -20,13 +20,13 @@ export const FormControlApp = (props: IProps) => {
     <Tooltip {...props} classes={{ popper: className }} arrow />
   ))(({ theme }) => ({
     [`& .${tooltipClasses.tooltip}`]: {
-      backgroundColor: "#fff",
-      color: "#707070",
+      backgroundColor: '#fff',
+      color: '#707070',
       maxWidth: 317,
       fontSize: theme.typography.pxToRem(12),
-      borderRadius: "6px",
-      border: "1px solid #dadde9",
-      padding: "18px 20px",
+      borderRadius: '6px',
+      border: '1px solid #dadde9',
+      padding: '18px 20px',
     },
   }));
 
@@ -39,17 +39,17 @@ export const FormControlApp = (props: IProps) => {
             error={props.msg ? true : false}
             shrink={true}
           >
-            {props.label}{" "}
+            {props.label}{' '}
             <span
               style={{
-                fontWeight: "bold",
-                fontSize: "20px",
-                position: "relative",
-                top: "4px",
+                fontWeight: 'bold',
+                fontSize: '20px',
+                position: 'relative',
+                top: '4px',
               }}
             >
-              {props.required && "*"}
-            </span>{" "}
+              {props.required && '*'}
+            </span>{' '}
             {props.icon}
             {props.tooltip && (
               <HtmlTooltip
@@ -57,7 +57,7 @@ export const FormControlApp = (props: IProps) => {
                   <>
                     <Typography
                       color="primary"
-                      sx={{ fontWeight: "700", fontSize: "16px" }}
+                      sx={{ fontWeight: '700', fontSize: '16px' }}
                     >
                       {props.tooltip}
                     </Typography>
@@ -65,16 +65,16 @@ export const FormControlApp = (props: IProps) => {
                       <>
                         <div
                           style={{
-                            width: "100%",
-                            height: "1px",
-                            borderRadius: "10px",
-                            margin: "10px 0",
-                            background: "#E2E2E2",
+                            width: '100%',
+                            height: '1px',
+                            borderRadius: '10px',
+                            margin: '10px 0',
+                            background: '#E2E2E2',
                           }}
                         />
                         <Typography
                           color="#707070"
-                          sx={{ fontWeight: "400", fontSize: "14px" }}
+                          sx={{ fontWeight: '400', fontSize: '14px' }}
                         >
                           {props.tooltipText}
                         </Typography>
@@ -85,14 +85,14 @@ export const FormControlApp = (props: IProps) => {
               >
                 <IconButton
                   sx={{
-                    padding: "0 0 2px 0",
-                    marginLeft: "14px",
-                    position: "relative",
-                    top: "0px",
+                    padding: '0 0 2px 0',
+                    marginLeft: '14px',
+                    position: 'relative',
+                    top: '0px',
                   }}
                 >
                   <InfoOutlinedIcon
-                    sx={{ fontSize: "20px", color: "#FFBA00" }}
+                    sx={{ fontSize: '20px', color: '#FFBA00' }}
                   />
                 </IconButton>
               </HtmlTooltip>
@@ -102,9 +102,9 @@ export const FormControlApp = (props: IProps) => {
       )}
       {props.children}
       {!props.noGutter && (
-        <Typography variant="caption" color="error" sx={{ fontSize: "10px" }}>
+        <Typography variant="caption" color="error" sx={{ fontSize: '10px' }}>
           {props.msg}
-          {"\u00A0"}
+          {'\u00A0'}
         </Typography>
       )}
     </FormControl>
