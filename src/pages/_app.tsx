2483e6b50e827a81b5e5d41bbbbf7bd482cae496
Gustavo Valverde <g.valverde02@gmail.com>
2023-06-17 23:37:03 -0400

    test(recaptcha): hardcoded key

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -24,6 +24,7 @@ export default function App({ Component, pageProps }: AppProps) {
         <CssBaseline />
         <Layout>
           <ReCaptchaProvider
+            reCaptchaKey="6LeCFYMmAAAAABhOLfiTI7n9g1aV5kr6TfgpslIa"
             useEnterprise
           >
             <Component {...pageProps} />
