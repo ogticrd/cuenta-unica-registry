9f654d1e99c7d7c92463927201b730c27ebf8910
Gustavo Valverde <g.valverde02@gmail.com>
2023-07-01 09:43:20 -0400

    refactor(analytics): use GTM instad of GTAG

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,11 @@
 import Head from 'next/head';
 import CssBaseline from '@mui/material/CssBaseline';
 import { ThemeProvider } from '@mui/material/styles';
+import { useEffect } from 'react';
 import type { AppProps } from 'next/app';
 import { Amplify } from 'aws-amplify';
 import { ReCaptchaProvider } from 'next-recaptcha-v3';
-import Script from 'next/script';
+import TagManager from 'react-gtm-module';
 
 import { SnackbarProvider } from '../components/elements/alert';
 import Layout from '../components/layout';
@@ -17,24 +18,14 @@ import '@/styles/globals.css';
 Amplify.configure(awsExports);
 
 export default function App({ Component, pageProps }: AppProps) {
+  // Google Tag Manager
+  useEffect(() => {
+    TagManager.initialize({
+      gtmId: `'${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}'`,
+    });
+  }, []);
   return (
     <>
-      <Script
-        strategy="lazyOnload"
-        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
-      />
-
-      <Script id="gtag-init" strategy="lazyOnload">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
-          page_path: window.location.pathname,
-          });
-        `}
-      </Script>
-
       <Head>
         <title>Plataforma Única de Autenticación</title>
         <meta
