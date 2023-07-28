1d45304df895a340bfa6f79771e1a0570b3fad42
Gustavo Valverde <gustavo@iterativo.do>
2023-07-28 14:44:43 +0100

    ref(ory): use schema `Citizens Schema v0.3.4` (#63)
    * ref(ory): use schema `Citizens Schema v0.2.0`
    
    * fix(api): add firstname
    
    * fix(ory): correct schema adaptations
    
    * fix(schema): `emails` is an array
    
    * fix(schema): firstName is name
    
    * fix(schema): revert email array
    
    * ref(ory): adapting props to `Schema v0.3.4`
    
    ---------
    
    Co-authored-by: Marluan Espiritusanto <marluanespiritusantoguerrero@gmail.com>

--- src/pages/api/types/index.ts
+++ src/pages/api/types/index.ts
@@ -2,6 +2,8 @@ export type {
   CitizenBasicInformation,
   CitizensBasicInformationResponse,
   CitizensTokenResponse,
+  CitizenBirthInformation,
+  CitizensBirthInformationResponse,
 } from './citizens.type';
 export type { Identity } from './iam.type';
 export type { ReCaptchaResponse } from './recaptcha.type';
