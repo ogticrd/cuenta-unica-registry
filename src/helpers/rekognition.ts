0e7ef7e2d723c6dd6e7bad780706c41eb2b833c8
Gustavo Valverde <g.valverde02@gmail.com>
2023-07-30 22:06:06 +0100

    fix(rekognition): configure using environment variables

--- src/helpers/rekognition.ts
+++ src/helpers/rekognition.ts
@@ -2,7 +2,15 @@ import { Amplify, withSSRContext } from 'aws-amplify';
 import { Rekognition } from '@aws-sdk/client-rekognition';
 import { NextApiRequest } from 'next/types';
 
-Amplify.configure({ ssr: true });
+const awsConfig = {
+  aws_project_region: process.env.AWS_REGION,
+  aws_cognito_identity_pool_id: process.env.AMPLIFY_IDENTITYPOOL_ID,
+  aws_cognito_region: process.env.AWS_REGION, // This could also be a separate environment variable if needed
+  aws_user_pools_id: process.env.AMPLIFY_USERPOOL_ID,
+  aws_user_pools_web_client_id: process.env.AMPLIFY_WEBCLIENT_ID,
+};
+
+Amplify.configure({ ...awsConfig, ssr: true });
 
 export async function getRekognitionClient(
   req: NextApiRequest
@@ -11,6 +19,7 @@ export async function getRekognitionClient(
   const credentials = await SSR.Credentials.get();
 
   const rekognitionClient = new Rekognition({
+    region: process.env.AWS_REGION,
     credentials,
   });
 
