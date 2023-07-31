95489664fb1544f87c2c633b8eb6eff5435ca23b
Marluan Espiritusanto <marluanespiritusantoguerrero@gmail.com>
2023-07-31 15:57:30 +0200

    rem: amplify initialization

--- src/helpers/rekognition.ts
+++ src/helpers/rekognition.ts
@@ -1,16 +1,6 @@
-import { Amplify, withSSRContext } from 'aws-amplify';
 import { Rekognition } from '@aws-sdk/client-rekognition';
+import {  withSSRContext } from 'aws-amplify';
 import { NextApiRequest } from 'next/types';
-
-const awsConfig = {
-  aws_project_region: process.env.AWS_REGION,
-  aws_cognito_identity_pool_id: process.env.AMPLIFY_IDENTITYPOOL_ID,
-  aws_cognito_region: process.env.AWS_REGION, // This could also be a separate environment variable if needed
-  aws_user_pools_id: process.env.AMPLIFY_USERPOOL_ID,
-  aws_user_pools_web_client_id: process.env.AMPLIFY_WEBCLIENT_ID,
-};
-
-Amplify.configure({ ...awsConfig, ssr: true });
 
 export async function getRekognitionClient(
   req: NextApiRequest
@@ -19,7 +9,7 @@ export async function getRekognitionClient(
   const credentials = await SSR.Credentials.get();
 
   const rekognitionClient = new Rekognition({
-    region: awsConfig.aws_project_region,
+    region: process.env.AWS_REGION,
     credentials,
   });
 
