9465fe63729104bd2caa941617b1d15e1f35214d
Gustavo Valverde <g.valverde02@gmail.com>
2023-08-03 11:32:11 +0100

    refactor(biometric): use translation keys instead of hardcoded strings

--- src/components/biometric/displayText.ts
+++ src/components/biometric/displayText.ts
@@ -1,81 +1,49 @@
-export type HintDisplayText = {
-  hintMoveFaceFrontOfCameraText?: string;
-  hintTooManyFacesText?: string;
-  hintFaceDetectedText?: string;
-  hintCanNotIdentifyText?: string;
-  hintTooCloseText?: string;
-  hintTooFarText?: string;
-  hintHoldFacePositionCountdownText?: string;
-  hintConnectingText?: string;
-  hintVerifyingText?: string;
-  hintIlluminationTooBrightText?: string;
-  hintIlluminationTooDarkText?: string;
-  hintIlluminationNormalText?: string;
-  hintHoldFaceForFreshnessText?: string;
+import {
+  LivenessDisplayText,
+  HintDisplayText,
+  CameraDisplayText,
+  InstructionDisplayText,
+  StreamDisplayText,
+  ErrorDisplayText,
+} from '@aws-amplify/ui-react-liveness/dist/types/components/FaceLivenessDetector/displayText';
+
+import { getDisplayText } from '@aws-amplify/ui-react-liveness/dist/types/components/FaceLivenessDetector/utils/getDisplayText';
+
+export const defaultHintDisplayText: Required<HintDisplayText> = {
+  hintMoveFaceFrontOfCameraText: 'Posicione la cara frente a la cámara',
+  hintTooManyFacesText: 'Asegúrese que solo una cara esté frente a la cámara',
+  hintFaceDetectedText: 'Cara detectada',
+  hintCanNotIdentifyText: 'No podemos identificar su cara frente a la cámara',
+  hintTooCloseText: 'Aléjese un poco más',
+  hintTooFarText: 'Acérquese un poco más',
+  hintHoldFacePositionCountdownText: 'Mantenga la posición de la cara',
+  hintConnectingText: 'Conectando...',
+  hintVerifyingText: 'Verificando...',
+  hintIlluminationTooBrightText: 'El área está muy iluminada',
+  hintIlluminationTooDarkText: 'El área está muy oscura',
+  hintIlluminationNormalText: 'Condiciones de iluminación correctas',
+  hintHoldFaceForFreshnessText: 'Quédese quieto',
 };
 
-export type CameraDisplayText = {
-  cameraMinSpecificationsHeadingText?: string;
-  cameraMinSpecificationsMessageText?: string;
-  cameraNotFoundHeadingText?: string;
-  cameraNotFoundMessageText?: string;
-  retryCameraPermissionsText?: string;
+export const defaultCameraDisplayText: Required<CameraDisplayText> = {
+  cameraMinSpecificationsHeadingText:
+    'La cámara no cumple con las especificaciones mínimas',
+  cameraMinSpecificationsMessageText:
+    'La cámara debe admitir una resolución de al menos 320 * 240 y 15 fotogramas por segundo.',
+  cameraNotFoundHeadingText: 'Cámara no accesible',
+  cameraNotFoundMessageText:
+    'Verifique que la cámara esté conectada y que los permisos de la cámara estén habilitados en la configuración antes de volver a intentarlo.',
+  retryCameraPermissionsText: 'Procesar de nuevo',
 };
 
-export type InstructionDisplayText = {
-  instructionsHeaderHeadingText?: string;
-  instructionsHeaderBodyText?: string;
-  instructionsBeginCheckText?: string;
-  photosensitivyWarningHeadingText?: string;
-  photosensitivyWarningBodyText?: string;
-  photosensitivyWarningInfoText?: string;
-  instructionListHeadingText?: string;
-  goodFitCaptionText?: string;
-  goodFitAltText?: string;
-  tooFarCaptionText?: string;
-  tooFarAltText?: string;
-  instructionListStepOneText?: string;
-  instructionListStepTwoText?: string;
-  instructionListStepThreeText?: string;
-  instructionListStepFourText?: string;
-};
-
-export type StreamDisplayText = {
-  recordingIndicatorText?: string;
-  cancelLivenessCheckText?: string;
-};
-
-export const defaultErrorDisplayText = {
-  timeoutHeaderText: 'Se acabó el tiempo',
-  timeoutMessageText:
-    'La cara no llenó el óvalo dentro del límite de tiempo. Vuelva a intentarlo y llene completamente el óvalo con la cara en 7 segundos.',
-  faceDistanceHeaderText: 'Comprobación fallida durante la cuenta regresiva',
-  faceDistanceMessageText:
-    'Evite acercarse durante la cuenta regresiva y asegúrese de que solo haya una cara frente a la cámara.',
-  clientHeaderText: 'Error del cliente',
-  clientMessageText: 'Verificación fallida debido a un problema con el cliente',
-  serverHeaderText: 'Problema del servidor',
-  serverMessageText:
-    'No se puede completar la verificación debido a un problema con el servidor',
-  landscapeHeaderText: 'Orientación horizontal no compatible',
-  landscapeMessageText:
-    'Gire su dispositivo a la orientación vertical (retrato).',
-  portraitMessageText:
-    'Asegúrese de que su dispositivo permanezca en orientación vertical (retrato) durante la verificación.',
-  tryAgainText: 'Intentar otra vez',
-};
-
-export type ErrorDisplayTextFoo = typeof defaultErrorDisplayText;
-export type ErrorDisplayText = Partial<ErrorDisplayTextFoo>;
-
-export const defaultLivenessDisplayText: Required<LivenessDisplayText> = {
-  instructionsHeaderHeadingText: 'Prueba de vida',
+export const defaultInstructionDisplayText: Required<InstructionDisplayText> = {
+  instructionsHeaderHeadingText: 'Verificación facial',
   instructionsHeaderBodyText:
     'Ahora va a realizar un proceso de verificación facial para demostrar que es una persona real.',
-  instructionsBeginCheckText: 'Iniciar prueba',
+  instructionsBeginCheckText: 'Iniciar verificación',
   photosensitivyWarningHeadingText: 'Alerta de fotosensibilidad',
   photosensitivyWarningBodyText:
-    'Esta comprobación muestra luces de colores. Tenga cuidado si es fotosensible.',
+    'Esta verificación muestra luces de colores. Tenga cuidado si es fotosensible.',
   photosensitivyWarningInfoText:
     'Un pequeño porcentaje de personas puede experimentar convulsiones epilépticas cuando se exponen a luces de colores. Tenga cuidado si usted o alguien de su familia tiene alguna condición epiléptica.',
   instructionListHeadingText:
@@ -93,36 +61,40 @@ export const defaultLivenessDisplayText: Required<LivenessDisplayText> = {
     'Asegúrese de que su cara no esté cubierta con gafas de sol o una máscara.',
   instructionListStepFourText:
     'Muévase a un lugar bien iluminado que no esté expuesto a la luz solar directa.',
-  cameraMinSpecificationsHeadingText:
-    'La cámara no cumple con las especificaciones mínimas',
-  cameraMinSpecificationsMessageText:
-    'La cámara debe admitir una resolución de al menos 320 * 240 y 15 fotogramas por segundo.',
-  cameraNotFoundHeadingText: 'Cámara no accesible',
-  cameraNotFoundMessageText:
-    'Verifique que la cámara esté conectada y que los permisos de la cámara estén habilitados en la configuración antes de volver a intentarlo.',
-  retryCameraPermissionsText: 'Procesar de nuevo',
-  cancelLivenessCheckText: 'Cancelar prueba de vida',
+};
+
+export const defaultStreamDisplayText: Required<StreamDisplayText> = {
   recordingIndicatorText: 'Grabando',
-  hintMoveFaceFrontOfCameraText: 'Posicione la cara frente a la cámara',
-  hintTooManyFacesText: 'Asegúrese que solo una cara esté frente a la cámara',
-  hintFaceDetectedText: 'Cara detectada',
-  hintCanNotIdentifyText: 'No podemos identificar su cara frente a la cámara',
-  hintTooCloseText: 'Muévase más atrás',
-  hintTooFarText: 'Muévase más cerca',
-  hintHoldFacePositionCountdownText:
-    'Mantenga la posición de la cara durante la cuenta regresiva',
-  hintConnectingText: 'Conectando...',
-  hintVerifyingText: 'Verificando...',
-  hintIlluminationTooBrightText: 'Muévase a un área con menos iluminación',
-  hintIlluminationTooDarkText: 'Muévase a un área con mayor iluminación',
-  hintIlluminationNormalText: 'Condiciones de iluminación correctas',
-  hintHoldFaceForFreshnessText: 'Quédese quieto',
+  cancelLivenessCheckText: 'Cancelar',
+};
+
+export const defaultErrorDisplayText: Required<ErrorDisplayText> = {
+  timeoutHeaderText: 'Se acabó el tiempo',
+  timeoutMessageText:
+    'La cara no llenó el óvalo dentro del límite de tiempo. Vuelva a intentarlo y llene completamente el óvalo con la cara en 7 segundos.',
+  faceDistanceHeaderText: 'Se detectó movimiento de la cara',
+  faceDistanceMessageText: 'Evite acercarse durante la conexión.',
+  multipleFacesHeaderText: 'Se detectaron múltiples caras',
+  multipleFacesMessageText: 'Solo una cara debe estar frente a la cámara.',
+  clientHeaderText: 'Error del cliente',
+  clientMessageText: 'Verificación fallida debido a un problema con el cliente',
+  serverHeaderText: 'Problema con el servidor',
+  serverMessageText:
+    'No se puede completar la verificación debido a un problema con el servidor',
+  landscapeHeaderText: 'Orientación horizontal no compatible',
+  landscapeMessageText:
+    'Gire su dispositivo a la orientación vertical (retrato).',
+  portraitMessageText:
+    'Asegúrese de que su dispositivo permanezca en orientación vertical (retrato) durante la verificación.',
+  tryAgainText: 'Intentar otra vez',
+};
+
+const defaultLivenessDisplayText: Required<LivenessDisplayText> = {
+  ...defaultHintDisplayText,
+  ...defaultCameraDisplayText,
+  ...defaultInstructionDisplayText,
+  ...defaultStreamDisplayText,
   ...defaultErrorDisplayText,
 };
 
-export interface LivenessDisplayText
-  extends HintDisplayText,
-    CameraDisplayText,
-    InstructionDisplayText,
-    ErrorDisplayText,
-    StreamDisplayText {}
+export const displayText = getDisplayText(defaultLivenessDisplayText);
