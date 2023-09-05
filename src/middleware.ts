c4bec7dcb2d2eac86474ed1c8a4a1dbae62ad317
Marluan Espiritusanto <marluanespiritusantoguerrero@gmail.com>
2023-09-05 16:31:22 +0200

    feat: edge-csrf middleware applied

---
+++ src/middleware.ts
@@ -0,0 +1,27 @@
+import { NextResponse, NextRequest } from 'next/server';
+import csrf from 'edge-csrf';
+
+const csrfProtect = csrf({
+  cookie: {
+    secure: process.env.NODE_ENV === 'production',
+    maxAge: 60 * 60 * 10,
+  },
+});
+
+export async function middleware(request: NextRequest) {
+  const response = NextResponse.next();
+
+  // csrf protection
+  const csrfError = await csrfProtect(request, response);
+
+  // check result
+  if (csrfError) {
+    return new NextResponse('invalid csrf token', { status: 403 });
+  }
+
+  return response;
+}
+
+export const config = {
+  matcher: '/api/:path*',
+};
