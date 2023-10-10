e46de9ea8a640e69ead4cedc48395bbbe243c366
Victor Jesús Rosario Vásquez <46900196+Victor1890@users.noreply.github.com>
2023-10-10 16:14:40 -0400

    Adding file next route validation (#100)
    * improve middleware next.js
    
    * adding prettier rule in eslint config
    
    ---------
    
    Co-authored-by: Marluan Espiritusanto <marluanespiritusanto@gmail.com>

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,15 @@
 import { NextResponse, type NextRequest } from 'next/server';
 
+const isFileRegex = /(api|_next\/static|_next\/image|favicon\.ico|\/_next)/;
+
 export async function middleware(request: NextRequest) {
+  const url = request.nextUrl.clone();
   const response = NextResponse.next();
+
+  if (url.pathname.indexOf('.') != -1 || typeof window !== 'undefined')
+    return response;
+  if (isFileRegex.test(url.pathname)) return response;
+
   const token = request.cookies.get('token');
 
   if (!token || token.value !== process.env.SITE_COOKIE_KEY) {
