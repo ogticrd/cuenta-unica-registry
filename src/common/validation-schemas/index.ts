9b3cf926bb2c7c1af2d9150e33198c7eb92179c7
Marluan Espiritusanto <marluanespiritusanto@gmail.com>
2023-11-01 20:27:40 +0100

    feat(ory): add account verification page (#122)
    * feat(ory): verification page and base logic
    
    * fix: extracting flow correctly
    
    * fix: redirecting on account verification

--- src/common/validation-schemas/index.ts
+++ src/common/validation-schemas/index.ts
@@ -1,3 +1,4 @@
+export { createVerificationSchema } from './verification.schema';
 export { createRegisterSchema } from './register.schema';
 export { createCedulaSchema } from './cedula.schema';
 export { createTermsSchema } from './terms.schema';
