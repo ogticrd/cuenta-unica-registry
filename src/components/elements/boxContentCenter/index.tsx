4c074451cc142b7e26b6debf0671e097af526de1
je1999 <je99alvarez@gmail.com>
2023-12-12 13:04:24 -0400

    fix(style): fix formatting in boxContentCenter
    bug fixed to comply with prettier formatting rules.

--- src/components/elements/boxContentCenter/index.tsx
+++ src/components/elements/boxContentCenter/index.tsx
@@ -5,9 +5,7 @@ import styles from './styles.module.css';
 export default function BoxContentCenter({ children }: any) {
   return (
     <Box className={styles.box_container}>
-      <Box className={styles.box_content}>
-        {children}
-      </Box>
+      <Box className={styles.box_content}>{children}</Box>
     </Box>
   );
 }
