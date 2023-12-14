629e7030fc13ddc5f0933eed09a8474e89013939
Jeffrey Mesa <JeffreyArt@hotmail.com>
2023-12-14 11:12:23 -0400

    fix(register): password checks (#145)
    * fix(register): check if the ID is in the password
    
    * fix(register): remove an instruction that prevent alerts on bad requests
    
    * fix(sentry): send complete error objects
    
    * fix: interaction response
    
    * ci: bump version

--- src/common/validation-schemas/register.schema.ts
+++ src/common/validation-schemas/register.schema.ts
@@ -2,13 +2,24 @@ import { z } from 'zod';
 
 import { Context } from '@/app/[lang]/provider';
 
-export const createRegisterSchema = ({ intl: { validations } }: Context) =>
+export const createRegisterSchema = (
+  { intl: { validations } }: Context,
+  cedula: string,
+) =>
   z
     .object({
       email: z.string().email(validations.email.invalid),
       emailConfirm: z.string().email(validations.email.invalid),
       password: z.string().min(8),
       passwordConfirm: z.string().min(8),
+    })
+    .refine(({ password }) => !password.includes(cedula), {
+      message: validations.password.hasID,
+      path: ['password'],
+    })
+    .refine(({ passwordConfirm }) => !passwordConfirm.includes(cedula), {
+      message: validations.password.hasID,
+      path: ['passwordConfirm'],
     })
     .refine((data) => data.email === data.emailConfirm, {
       message: validations.email.noMatch,
