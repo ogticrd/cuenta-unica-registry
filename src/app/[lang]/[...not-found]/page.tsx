c44d31558e6dd5ba005de830e3552188d3f3c7bc
je1999 <je99alvarez@gmail.com>
2023-12-15 14:12:43 -0400

    chore: fix format error with prettier

--- src/app/[lang]/[...not-found]/page.tsx
+++ src/app/[lang]/[...not-found]/page.tsx
@@ -13,42 +13,42 @@ import { ButtonApp } from '@/components/elements/button';
 import { useLanguage } from '@/app/[lang]/provider';
 
 export default function NotFound() {
-    const router = useRouter();
-    const { intl } = useLanguage();
+  const router = useRouter();
+  const { intl } = useLanguage();
 
-    return (
-        <GridContainer>
-            <GridItem md={12} lg={12}>
-                <div style={{ display: 'flex', justifyContent: 'center' }}>
-                    <Image
-                        src={NotFound404?.src}
-                        alt="img 404"
-                        width="231"
-                        height="225"
-                    />
-                </div>
-                <br />
-                <Typography
-                    color="primary"
-                    sx={{
-                        fontSize: '24px',
-                        fontWeight: '700',
-                        textAlign: 'center',
-                    }}
-                    gutterBottom
-                >
-                    {intl.notFound.title}
-                </Typography>
-                <TextBody textCenter gutterBottom>
-                    {intl.notFound.description}
-                </TextBody>
-            </GridItem>
+  return (
+    <GridContainer>
+      <GridItem md={12} lg={12}>
+        <div style={{ display: 'flex', justifyContent: 'center' }}>
+          <Image
+            src={NotFound404?.src}
+            alt="img 404"
+            width="231"
+            height="225"
+          />
+        </div>
+        <br />
+        <Typography
+          color="primary"
+          sx={{
+            fontSize: '24px',
+            fontWeight: '700',
+            textAlign: 'center',
+          }}
+          gutterBottom
+        >
+          {intl.notFound.title}
+        </Typography>
+        <TextBody textCenter gutterBottom>
+          {intl.notFound.description}
+        </TextBody>
+      </GridItem>
 
-            <GridItem md={12} lg={12}>
-                <ButtonApp onClick={() => router.push('/')}>
-                    {intl.notFound.returnHome}
-                </ButtonApp>
-            </GridItem>
-        </GridContainer>
-    )
+      <GridItem md={12} lg={12}>
+        <ButtonApp onClick={() => router.push('/')}>
+          {intl.notFound.returnHome}
+        </ButtonApp>
+      </GridItem>
+    </GridContainer>
+  );
 }
