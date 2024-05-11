33b608c89c3060184ea608483f52385983575a00
Jeffrey Mesa <jeffreyart@hotmail.com>
2024-05-11 15:37:56 -0400

    refactor: add last name boolean filtering
    fixes the " undefined" on `last` trait when second last name was `undefined`

--- src/app/[lang]/register/form.tsx
+++ src/app/[lang]/register/form.tsx
@@ -145,7 +145,9 @@ export function Form({ cedula }: Props) {
           username: citizen.id,
           name: {
             first: citizen.names,
-            last: `${citizen.firstSurname} ${citizen.secondSurname}`,
+            last: [citizen.firstSurname, citizen.secondSurname]
+              .filter(Boolean)
+              .join(' '),
           },
           birthdate: citizen.birthDate,
           gender: citizen.gender,
