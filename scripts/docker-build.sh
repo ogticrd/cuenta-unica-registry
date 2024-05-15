bbb368f4f5ae9383a2564c531912f66bf35c2a7f
Jeffrey Mesa <JeffreyArt@hotmail.com>
2024-05-15 16:11:25 -0400

    fix: revert next `14.1.1` -> `13.5.6` (#224)
    * fix: revert next `14.1.1` -> `13.5.6`
    
    * ci: add image build script
    
    - useful for testing
    
    * ci: use `pnpm` in command (#226)
    
    * fix: revert next `14.1.1` -> `13.5.6`
    
    * ci: add image build script
    
    - useful for testing
    
    * deps: update app dependencies
    
    * chore: upgrade husky
    
    - `8.0.3` -> `9.0.11`
    
    * ci: use native audit
    
    * ci: ignore CVE-2024-34351 until next upgrade is feasible

---
+++ scripts/docker-build.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+IMAGE_NAME=cuc-registry:latest
+AWS_EXPORTS_PATH=./src/aws-exports.js
+
+# to use the variables, run this in your terminal
+#   export $(grep -v '^#' .env | xargs)
+ 
+docker build \
+  --secret id=AWS_EXPORTS_JSON,src=$AWS_EXPORTS_PATH \
+  --build-arg SENTRY_ORG=$SENTRY_ORG \
+  --build-arg SENTRY_PROJECT=$SENTRY_PROJECT \
+  --build-arg SENTRY_AUTH_TOKEN=$SENTRY_AUTH_TOKEN \
+  --build-arg NEXT_PUBLIC_SENTRY_DSN=$NEXT_PUBLIC_SENTRY_DSN \
+  --build-arg NEXT_PUBLIC_RECAPTCHA_SITE_KEY=$NEXT_PUBLIC_RECAPTCHA_SITE_KEY \
+  -t $IMAGE_NAME .
+
+unset IMAGE_NAME
