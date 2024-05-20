a90a7fe866d91cd1b89169185fb971a033e26926
Jeffrey Mesa <jeffreyart@hotmail.com>
2024-05-20 19:19:42 -0400

    chore: update `next`

--- next.config.js
+++ next.config.js
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    serverActions: true,
-  },
   reactStrictMode: true,
   output: 'standalone',
   webpack: (config, { webpack, isServer, nextRuntime }) => {
