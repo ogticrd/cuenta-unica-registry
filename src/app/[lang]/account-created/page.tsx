d425823ce48759aa6342250a374ee4803896b12b
Jeffrey Mesa <jeffreyart@hotmail.com>
2024-07-01 14:28:14 -0400

    refactor(account-created): add sites of interest
    closes #242

--- src/app/[lang]/account-created/page.tsx
+++ src/app/[lang]/account-created/page.tsx
@@ -1,27 +1,53 @@
-'use client';
-
 import { Typography } from '@mui/material';
 import Image from 'next/image';
+import Link from 'next/link';
 
 import AccountCreated from '@public/assets/account-created.svg';
+import becas from '@public/assets/becas.svg';
+import gobdo from '@public/assets/gobdo.svg';
+import soyyo from '@public/assets/soyyo.svg';
+import styles from './page.module.css';
 
 import { GridContainer, GridItem } from '@/components/elements/grid';
 import { TextBody } from '@/components/elements/typography';
-import { ButtonApp } from '@/components/elements/button';
-import { useLanguage } from '@/app/[lang]/provider';
+import { getDictionary } from '@/dictionaries';
 
-export default function ConfirmationPage() {
-  const { intl } = useLanguage();
+export default async function ConfirmationPage({ params: { lang } }) {
+  const intl = await getDictionary(lang);
+
+  const sites = [
+    {
+      ready: true,
+      name: 'gob.do',
+      url: 'https://www.gob.do',
+      description: intl.registered.descriptions.gobdo,
+      icon: gobdo,
+    },
+    {
+      ready: false,
+      name: 'Beca tu Futuro',
+      url: 'https://becas.gob.do',
+      description: intl.registered.descriptions.becas,
+      icon: becas,
+    },
+    {
+      ready: false,
+      name: 'Soy Yo',
+      url: '#',
+      description: intl.registered.descriptions.soyyo,
+      icon: soyyo,
+    },
+  ];
 
   return (
     <GridContainer>
       <GridItem md={12} lg={12}>
-        <div style={{ display: 'flex', justifyContent: 'center' }}>
+        <div className={styles.center}>
           <Image
             src={AccountCreated?.src}
             alt="imagen de cuenta creada"
-            width="259"
-            height="225"
+            width={259}
+            height={225}
           />
         </div>
         <br />
@@ -42,12 +68,48 @@ export default function ConfirmationPage() {
       </GridItem>
 
       <GridItem md={12} lg={12}>
-        <ButtonApp
-          onClick={() => window.open('https://mi.cuentaunica.gob.do/ui/login')}
-        >
-          {intl.actions.myAccount}
-        </ButtonApp>
+        <div className={styles.center}>
+          <Typography
+            color="primary"
+            fontWeight={500}
+            fontSize="medium"
+            bgcolor="#0091ff20"
+            sx={{ px: 2, py: 1 }}
+            borderRadius={2}
+          >
+            {intl.registered.capabilities}
+          </Typography>
+        </div>
       </GridItem>
+
+      {sites.map((site, index) => (
+        <GridItem md={12} lg={12} key={index} sx={{ mt: 1 }}>
+          <Link
+            className={styles.listitem}
+            href={site.ready ? site.url : '#'}
+            target="_blank"
+            aria-disabled={!site.ready}
+            data-ready={site.ready}
+          >
+            <Image
+              alt={site.name}
+              width={110}
+              height={60}
+              src={site.icon?.src}
+              className={styles.listimage}
+            />
+            <div className={styles.listcaption}>
+              <Typography color="primary">
+                <strong>{site.name}, </strong>
+                <span style={{ opacity: '85%' }}>
+                  {site.description.toLowerCase()}
+                </span>
+              </Typography>
+            </div>
+            {site.ready ? null : <span>{intl.registered.soon}</span>}
+          </Link>
+        </GridItem>
+      ))}
     </GridContainer>
   );
 }
