b1ba6cc81504016146f804556956dda46e1b4293
Jeffrey Mesa <JeffreyArt@hotmail.com>
2024-09-02 17:35:03 -0400

    fix: use backoffice db to verify existence on iam error (#262)
    * fix: use backoffice db to verify existence on iam error
    
    * bump: `v1.4.2`

--- src/actions/iam.action.ts
+++ src/actions/iam.action.ts
@@ -10,11 +10,39 @@ export async function findIamCitizen(cedula: string) {
     }),
   );
 
-  const { data: identities } = await backend.listIdentities({
-    credentialsIdentifier: cedula,
-  });
+  const { data: identities } = await backend
+    .listIdentities({
+      credentialsIdentifier: cedula,
+    })
+    .catch(() => findAccountInBackoffice(cedula));
 
   return {
     exists: identities.length !== 0,
   };
 }
+
+async function findAccountInBackoffice(cedula: string) {
+  const url = new URL('v1/accounts', process.env.BACKOFFICE_API_URL);
+  url.searchParams.append('term', cedula);
+
+  const resp = await fetch(url, withCredentials());
+
+  if (resp.ok) {
+    return resp
+      .json()
+      .then((data: Array<BackofficeAccount>) =>
+        data.map((data) => ({ ...data, verifiable_addresses: [] })),
+      )
+      .then((data) => ({ data }));
+  }
+
+  await resp.json().then(console.error);
+
+  return { data: [] };
+}
+
+type BackofficeAccount = { id: string; cedula: string };
+
+const withCredentials = () => ({
+  headers: { 'x-account-apikey': `${process.env.BACKOFFICE_API_KEY}` },
+});
