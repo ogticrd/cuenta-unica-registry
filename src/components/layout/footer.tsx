e382805181f858d099156f523a378cd133849515
Jeffrey Mesa <jeffreyart@hotmail.com>
2025-01-27 12:47:40 -0400

    refactor(footer): enhance layout and improve accessibility

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -24,71 +24,142 @@ export default async function Footer({ intl }: Props) {
       <div className={styles.footer}>
         <div className={styles.footer_container}>
           <GridContainer spacing={4}>
-            <GridItem md={12} lg={2}>
-              <Image src={logoGOB.src} alt="logo" width="198" height="80" />
+            <GridItem md={4} lg={3}>
+              <GridContainer flexDirection="column">
+                <GridItem md={4} lg={3}>
+                  <Image
+                    src={logoGOB.src}
+                    alt="Logo Gobierno de la República Dominicana"
+                    width="198"
+                    height="80"
+                  />
+                </GridItem>
+                <GridItem md={4} lg={3}>
+                  <Image
+                    src={logoWhite.src}
+                    alt="Logo Cuenta Única"
+                    width="119"
+                    height="59"
+                    // style={{ marginLeft: '10px' }}
+                  />
+                </GridItem>
+              </GridContainer>
             </GridItem>
-            <GridItem md={12} lg={2}>
-              <Image src={logoWhite.src} alt="logo" width="166" height="82" />
-            </GridItem>
-            <GridItem md={12} lg={8}>
-              <GridContainer spacing={4}>
-                <GridItem md={6} lg={3}>
-                  <Typography fontWeight="500" fontSize={16} color="white">
+            <GridItem md={12} lg={9}>
+              <GridContainer spacing={2}>
+                {/* <GridItem md={6} lg={4}>
+                  <Typography fontWeight="600" fontSize={16} color="white">
                     {intl.footer.knowUs}
                   </Typography>
                   <br />
-                  <Typography color="white" fontWeight="400" fontSize="16">
+                  <Typography fontWeight="400" fontSize={15} color="#b3cbe6">
                     Oficina Gubernamental de Tecnologías de la Información y
                     Comunicación (OGTIC)
                   </Typography>
-                </GridItem>
+                </GridItem> */}
 
-                <GridItem md={6} lg={3}>
-                  <Typography fontWeight="500" fontSize={16} color="white">
-                    {intl.footer.contact}
-                  </Typography>
-                  <br />
-                  <Typography color="white" fontWeight="400" fontSize="16">
-                    Tel: (809)-286-1009
-                  </Typography>
-                  <Typography color="white" fontWeight="400" fontSize="16">
-                    Fax: (809)-732-5465
-                  </Typography>
-                  <Typography color="white" fontWeight="400" fontSize="16">
-                    info@ogtic.gob.do
-                  </Typography>
-                </GridItem>
-
-                <GridItem md={6} lg={3}>
-                  <Typography fontWeight="500" fontSize={16} color="white">
-                    {intl.footer.findUs}
-                  </Typography>
-                  <br />
-                  <Typography color="white" fontWeight="400" fontSize="16">
-                    Ave. Rómulo Betancourt #311, Edificio Corporativo Vista 311,
-                    Santo Domingo, República Dominicana.
-                  </Typography>
-                </GridItem>
-
-                <GridItem md={6} lg={3}>
-                  <Typography fontWeight="500" fontSize={16} color="white">
+                <GridItem md={3} lg={3}>
+                  <Typography
+                    fontWeight="600"
+                    fontSize={16}
+                    color="white"
+                    mb={1}
+                  >
                     {intl.footer.info}
                   </Typography>
-                  <br />
+
                   <Link
                     href="/terms"
                     target="_blank"
                     style={{ textDecoration: 'none' }}
                   >
-                    <Typography color="white" fontWeight="400" fontSize="16">
+                    <Typography
+                      color="white"
+                      fontWeight="400"
+                      fontSize={15}
+                      sx={{ lineHeight: '1.6' }}
+                    >
                       {intl.common.terms}
                     </Typography>
                   </Link>
-                  <Typography color="white" fontWeight="400" fontSize="16">
+                  <Typography
+                    color="white"
+                    fontWeight="400"
+                    fontSize={15}
+                    sx={{ lineHeight: '1.6' }}
+                  >
                     {intl.common.policy}
                   </Typography>
-                  <Typography color="white" fontWeight="400" fontSize="16">
+                  <Typography
+                    color="white"
+                    fontWeight="400"
+                    fontSize={15}
+                    sx={{ lineHeight: '1.6' }}
+                  >
                     {intl.common.faq}
+                  </Typography>
+                </GridItem>
+
+                <GridItem md={3} lg={3}>
+                  <Typography
+                    fontWeight="600"
+                    fontSize={16}
+                    color="white"
+                    mb={1}
+                  >
+                    {intl.footer.contact}
+                  </Typography>
+
+                  <Typography
+                    fontWeight="400"
+                    fontSize={15}
+                    color="#b3cbe6"
+                    sx={{ lineHeight: '1.6' }}
+                  >
+                    Tel: (809)-286-1009
+                  </Typography>
+                  <Typography
+                    fontWeight="400"
+                    fontSize={15}
+                    color="#b3cbe6"
+                    sx={{ lineHeight: '1.6' }}
+                  >
+                    Fax: (809)-732-5465
+                  </Typography>
+
+                  <Link
+                    href="mailto:info@ogtic.gob.do"
+                    style={{ textDecorationColor: '#b3cbe6' }}
+                  >
+                    <Typography
+                      fontWeight="400"
+                      fontSize={15}
+                      color="#b3cbe6"
+                      sx={{ lineHeight: '1.6' }}
+                    >
+                      info@ogtic.gob.do
+                    </Typography>
+                  </Link>
+                </GridItem>
+
+                <GridItem md={6} lg={4}>
+                  <Typography
+                    fontWeight="600"
+                    fontSize={16}
+                    color="white"
+                    mb={1}
+                  >
+                    {intl.footer.findUs}
+                  </Typography>
+
+                  <Typography
+                    fontWeight="400"
+                    fontSize={15}
+                    color="#b3cbe6"
+                    sx={{ lineHeight: '1.6' }}
+                  >
+                    Ave. Rómulo Betancourt #311, Edificio Corporativo Vista 311,
+                    Santo Domingo, República Dominicana.
                   </Typography>
                 </GridItem>
               </GridContainer>
@@ -97,20 +168,25 @@ export default async function Footer({ intl }: Props) {
         </div>
       </div>
 
-      <div style={{ background: 'white', padding: '12.5px 25px' }}>
+      <div style={{ background: 'white', padding: '0.4rem 2rem' }}>
         <div className={styles.footer_container}>
           <GridContainer>
             <GridItem md={6} lg={6}>
               <div style={{ marginTop: '8px' }}>
-                <Typography variant="caption" fontWeight="600" color="primary">
-                  © {new Date().getFullYear()} {intl.footer.rightsReserved}{' '}
+                <Typography
+                  variant="caption"
+                  fontWeight="600"
+                  color="primary"
+                  sx={{ fontFamily: 'sans-serif' }}
+                >
+                  &copy; {new Date().getFullYear()} {intl.footer.rightsReserved}{' '}
                   {intl.footer.developedBy}
                 </Typography>
                 <Link href="https://ogtic.gob.do/" target="_blank">
                   <Image
                     style={{
-                      marginBottom: '-10px',
-                      marginLeft: '5px',
+                      marginBottom: '-0.7rem',
+                      marginLeft: '0.2rem',
                       cursor: 'pointer',
                     }}
                     src={logoOGTIC.src}
