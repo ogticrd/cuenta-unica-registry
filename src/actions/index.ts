f28dbcafb9de7b49cd415d282b8a0c9479038d69
Jeffrey Mesa <jeffreyart@hotmail.com>
2025-02-06 11:02:43 -0400

    refactor: enhance jar management

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,5 +1,5 @@
+export { setCookie, getCookie, removeCookie } from './cookie.action';
 export { validateRecaptcha } from './recaptcha.action';
-export { setCookie, getCookie } from './cookie.action';
 export { verifyPassword } from './pwned.action';
 export { findCitizen } from './citizen.action';
 export { findIamCitizen } from './iam.action';
