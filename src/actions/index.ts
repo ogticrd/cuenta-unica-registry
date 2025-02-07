7b23201230de7c19687f6a84c496b9e9b71a0e0d
Jeffrey Mesa <jeffreyart@hotmail.com>
2025-02-07 14:19:26 -0400

    refactor: move password pwned logic into validation schema

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,5 +1,4 @@
 export { setCookie, getCookie, removeCookie } from './cookie.action';
 export { validateRecaptcha } from './recaptcha.action';
-export { verifyPassword } from './pwned.action';
 export { findCitizen } from './citizen.action';
 export { findIamCitizen } from './iam.action';
